4de0666694831c8390b40c80b14c204a01f22b56
Tomasz Skrzyp <Tomasz Skrzyp@DESKTOP-AMBO86B>
2025-04-27 13:05:51 +0200

    Updated email validation

--- backend/src/logic/validateCredentials.js
+++ backend/src/logic/validateCredentials.js
@@ -10,7 +10,7 @@ function validatePassword(password) {
 }
 
 function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
 export  { validateEmail, validatePassword, validateUsername };
