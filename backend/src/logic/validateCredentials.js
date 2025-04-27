fd6e9363dc55b4759e8c754293a0be18c72ad4f5
Tomasz Skrzyp <Tomasz Skrzyp@DESKTOP-AMBO86B>
2025-04-27 13:06:41 +0200

    Updated email validation

--- backend/src/logic/validateCredentials.js
+++ backend/src/logic/validateCredentials.js
@@ -10,6 +10,7 @@ function validatePassword(password) {
 }
 
 function validateEmail(email) {
+    if (!email || email.length > 150 || !email.includes('@')) return false;
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
